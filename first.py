b3d4fe52637700cff944ea11e314e0cf455cc89f
ArinaKorchagina <122806500+ArinaKorchagina@users.noreply.github.com>
2023-12-15 16:59:23 +0400

    Update first.py

--- first.py
+++ first.py
@@ -1,1 +1,60 @@
-print('My first git-repo')
+import os
+import sys
+import random
+
+import pygame
+
+# Изображение не получится загрузить
+# без предварительной инициализации pygame
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+all_sprites = pygame.sprite.Group()
+
+
+class Bomb(pygame.sprite.Sprite):
+    image = load_image("bomb.png")
+    image_boom = load_image("boom.png")
+
+    def __init__(self, *group):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
+        # Это очень важно!!!
+        super().__init__(*group)
+        self.image = Bomb.image
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(width)
+        self.rect.y = random.randrange(height)
+
+    def update(self, *args):
+        self.rect = self.rect.move(random.randrange(3) - 1,
+                                   random.randrange(3) - 1)
+        if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
+                self.rect.collidepoint(args[0].pos):
+            self.image = self.image_boom
+
+
+for _ in range(500):
+    Bomb(all_sprites)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        screen.fill((255, 0, 255))
+        all_sprites.draw(screen)
+        all_sprites.update(event)
+        pygame.display.flip()
+
